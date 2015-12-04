3cd39dc67a7a2444e5631b072377b275ec54a51d
Yennifer Santiago <yennifersantiago@gmail.com>
2015-12-04 09:52:23 -0430

    Culminado modulo basico website

--- models.py
+++ models.py
@@ -6,11 +6,15 @@ class Teachers(models.Model):
 
 	name = fields.Char()
 	biography = fields.Html()
-	course_ids = fields.One2many('academy.courses', 'teacher_id', string="Courses")
+
+	# course_ids = fields.One2many('academy.courses', 'teacher_id', string="Courses")
+
+	course_ids = fields.One2many('product.template', 'teacher_id', string="Courses")
 
 class Courses(models.Model):
-	_name = 'academy.courses'
-	_inherit = 'mail.thread'
+	# _name = 'academy.courses'
+	# _inherit = 'mail.thread'
+	_inherit = 'product.template'
 
 	name = fields.Char()
 	teacher_id = fields.Many2one('academy.teachers', string="Teacher")
